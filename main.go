d367fb97b8788d2917984785354652f3fef4fd9f
cuti <nhamtybv@yahoo.com.vn>
2024-01-30 08:31:48 +0700

    Implememted CRUD

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"html/template"
 	"log"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 	_ "github.com/mattn/go-sqlite3"
@@ -52,6 +53,8 @@ func main() {
 	r := mux.NewRouter()
 	r.HandleFunc("/", Home).Methods("GET")
 	r.HandleFunc("/tasks", Add).Methods("POST")
+	r.HandleFunc("/tasks/{id}/toggle", Toggle).Methods("PUT")
+	r.HandleFunc("/tasks/{id}", Del).Methods("DELETE")
 	log.Println("Listen and serve at :3000")
 	log.Fatal(http.ListenAndServe(":3000", r))
 }
@@ -126,8 +129,8 @@ func fetchTasks() (Tasks, error) {
 
 func statsTasks() (Stats, error) {
 	var st Stats
-	query := `select count(1) as num_of_tasks
-				   , sum(case when completed then 1 else 0 end) as completed_tasks
+	query := `select coalesce(count(1), 0) as num_of_tasks
+				   , coalesce(sum(case when completed then 1 else 0 end), 0) as completed_tasks
 				from tasks
 	`
 	err := db.QueryRow(query).Scan(&st.Count, &st.CompletedCount)
@@ -173,4 +176,92 @@ func Add(w http.ResponseWriter, r *http.Request) {
 	_ = tmpl.ExecuteTemplate(w, "add-form-block", nil)
 	_ = tmpl.ExecuteTemplate(w, "tasks-stats-block", st)
 	_ = tmpl.ExecuteTemplate(w, "task-item-block", task)
+}
+
+// Update tasks
+func updateTask(task Task) error {
+	query := `update tasks set title = ?, completed = ? where id = ?`
+	_, err := db.Exec(query, task.Title, task.Completed, task.ID)
+	log.Printf("updating: %d - %s - %t", task.ID, task.Title, task.Completed)
+	if err != nil {
+		log.Printf("updating task error: %v", err)
+		return err
+	}
+	return nil
+}
+
+func getTask(id int) (Task, error) {
+	query := `select id, title, completed from tasks where id = ?`
+	var task Task
+	err := db.QueryRow(query, id).Scan(&task.ID, &task.Title, &task.Completed)
+	if err != nil {
+		log.Printf("get task error: %v", err)
+		return task, err
+	}
+	return task, nil
+}
+
+func Toggle(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	taskId, err := strconv.Atoi(id)
+	if err != nil {
+		log.Printf("id not found: %v", err)
+		return
+	}
+	t, err := getTask(taskId)
+	if err != nil {
+		log.Printf("task not found: %v", err)
+		return
+	}
+	t.Completed = !t.Completed
+	err = updateTask(t)
+	if err != nil {
+		log.Printf("updating task error: %v", err)
+		return
+	}
+	t.OOB = "true"
+	ts, err := statsTasks()
+	if err != nil {
+		log.Printf("stats task error: %v", err)
+		return
+	}
+	_ = tmpl.ExecuteTemplate(w, "tasks-stats-block", ts)
+	_ = tmpl.ExecuteTemplate(w, "task-item-block", t)
+}
+
+// Delete task
+func deleteTask(id int) error {
+	query := `delete from tasks where id = ?`
+	_, err := db.Exec(query, id)
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func Del(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	taskId, err := strconv.Atoi(id)
+	if err != nil {
+		log.Printf("id not found: %v", err)
+		return
+	}
+	t, err := getTask(taskId)
+	if err != nil {
+		log.Printf("task not found: %v", err)
+		return
+	}
+	err = deleteTask(t.ID)
+	if err != nil {
+		log.Printf("delete task error: %v", err)
+		return
+	}
+	t.OOB = "delete"
+	ts, err := statsTasks()
+	if err != nil {
+		log.Printf("stats task error: %v", err)
+		return
+	}
+	_ = tmpl.ExecuteTemplate(w, "tasks-stats-block", ts)
+	_ = tmpl.ExecuteTemplate(w, "task-item-block", t)
 }
